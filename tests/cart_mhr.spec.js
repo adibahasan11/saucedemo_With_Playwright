ae64b4eaa6c36fcec27a595ef51515da23d55fab
Rumman003 <49231987+Rumman003@users.noreply.github.com>
2025-12-09 15:26:29 +0000

    Updated and fix the error

--- tests/cart_mhr.spec.js
+++ tests/cart_mhr.spec.js
@@ -5,12 +5,14 @@ import logger from '../helpers/logger.js';
 test.describe('Shopping Cart Functionality @smoke', () => {
     test('should display product in cart page', async ({ authenticatedPage }) => {
         const { dashboardPage, cartPage } = authenticatedPage;
+        // adde the comment resolver 1
 
         // Get product from Excel
         const product = excelReader.getProductByName('Sauce Labs Bolt T-Shirt');
 
         logger.step(`Test Case: Verify "${product.productName}" in cart page`);
-
+        // adde the comment resolver2
+        
         // Add product to cart
         await dashboardPage.addProductToCartByName(product.productName);
 
